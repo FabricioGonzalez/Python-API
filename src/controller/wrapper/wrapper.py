1214fa6f320fb929573b793e3ee6b6920220b754
Fabricio Gonzalez França Ribeiro <fabriciogonzalezfr@gmail.com>
2021-01-29 22:07:49 -0300

    Adiçao de uma funçao que cria respostas a requisiçoes

--- src/controller/wrapper/wrapper.py
+++ src/controller/wrapper/wrapper.py
@@ -2,5 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask("HTTP-API")
 
+def agente_de_respostas(dados):
+  
+  response = Flask.make_response(Flask,dados)
 
-
+  response.headers['Content-Type'] = 'application/json; charset=utf8'
+  
+  return response
