eb0d889acd05396e3c7e90125d413bbd98c1311b
Fabricio Gonzalez França Ribeiro <fabriciogonzalezfr@gmail.com>
2021-01-30 22:03:17 -0300

    terminado o Wrapper do framework

--- src/controller/wrapper/wrapper.py
+++ src/controller/wrapper/wrapper.py
@@ -3,9 +3,8 @@ from flask import Flask, request, jsonify
 app = Flask("HTTP-API")
 
 def agente_de_respostas(dados):
-  
-  response = Flask.make_response(Flask,dados)
 
+  response =   response = Flask.make_response(Flask,dados)
   response.headers['Content-Type'] = 'application/json; charset=utf8'
   
   return response
